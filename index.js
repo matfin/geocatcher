8783392d6833aaad87f9ef1521158430c8cc0514
Matt Finucane <matfin@gmail.com>
2015-07-25 20:46:43 +0200

    Writing unit tests for functions.

--- index.js
+++ index.js
@@ -1,99 +1,104 @@
 'use strict';
 
-module.exports = {
-	/**
-	 *	Fetch the data from an endpoint given the URI
-	 *	and execute a callback on success or failure
-	 *
-	 *	@param {String} uri - endpoint uri
-	 *	@param {String} cb 	- callback function to execute
-	 */
-	fetch: function(uri, cb) {
-		var https = require('https');
+/**
+ *	Fetch the data from an endpoint given the URI
+ *	and execute a callback on success or failure
+ *
+ *	@param {String} uri - endpoint uri
+ *	@param {String} cb 	- callback function to execute
+ */
+var fetch = function(uri, cb) {
+	var https = require('https');
 
-		https.get(uri, function(res) {
-			var body = '';
-			res.on('data', function(chunk) {
-				body += chunk;
-			});
-			res.on('end', function() {
-				if(typeof cb === 'function') {
-					cb(JSON.parse(body));
-				}
-			});
-		});	
-	},
-
-	/**
-	 *	Calculate distance using Haversine formula with two coordinates
-	 *
-	 *	@param {Object} source_coord - the source coordinate which should take the form {lat: 44.55555, lng: -5.33322}
-	 *	@param {Object} dest_coord - given coordinate to calculate against
-	 *	@return {Number} - the calculated distance in km
-	 */
-	haversineDistance: function(source_coord, dest_coord) {
-
-		if(Number.prototype.toRadians === undefined) {
-			Number.prototype.toRadians = function() {
-				return this * Math.PI / 180;
+	https.get(uri, function(res) {
+		var body = '';
+		res.on('data', function(chunk) {
+			body += chunk;
+		});
+		res.on('end', function() {
+			if(typeof cb === 'function') {
+				cb(JSON.parse(body));
 			}
+		});
+	});	
+};
+
+/**
+ *	Calculate distance using Haversine formula with two coordinates
+ *
+ *	@param {Object} source_coord - the source coordinate which should take the form {lat: 44.55555, lng: -5.33322}
+ *	@param {Object} dest_coord - given coordinate to calculate against
+ *	@return {Number} - the calculated distance in km
+ */
+var haversineDistance = function(source_coord, dest_coord) {
+
+	if(Number.prototype.toRadians === undefined) {
+		Number.prototype.toRadians = function() {
+			return this * Math.PI / 180;
 		}
-
-		var earth_radius = 6371,
-				source_lat_radians = source_coord.lat.toRadians(),
-				dest_lat_radians = dest_coord.lat.toRadians(),
-				lat_diff_radians = (dest_coord.lat - source_coord.lat).toRadians(),
-				lng_diff_radians = (dest_coord.lng - source_coord.lng).toRadians();
-
-		var formula = Math.sin(lat_diff_radians / 2) * Math.sin(lat_diff_radians / 2) +
-									Math.cos(source_lat_radians) * Math.cos(dest_lat_radians) *
-									Math.sin(lng_diff_radians / 2) * Math.sin(lng_diff_radians / 2),
-				ataned = 	2 * Math.atan2(Math.sqrt(formula), Math.sqrt(1 - formula));
-
-		return earth_radius * ataned;
-	},
-
-	/**
-	 *	Filter the fetched data by their distance
-	 *
-	 *	@param {Array} items - the fetched items from the json endpoint 
-	 *	@param {Object} source_location - the reference location used to measure distance
-	 *	@param {Number} within_distance - the given distance (in KM) to filter by
-	 *	@return {Array} - the items filtered by distance
-	 */
-	filterWithinDistance: function(items, source_location, within_distance) {
-		return items.filter(function(item) {
-			if(typeof item.latitude === 'undefined' || typeof item.longitude === 'undefined') {
-				return;
-			}
-			return module.exports.haversineDistance(source_location, {lat: item.latitude, lng: item.longitude}) <= within_distance;
-		});
-	},
-
-	/**
-	 *	Order and map a collection
-	 *	@param {Array} source - the unmapped and unsorted array
-	 *	@return {Array} dest - the mapped and sorted array
-	 */
-	sortAndMap: function(source) {
-		return source.sort(function(a, b){
-			return a.user_id > b.user_id;		
-		}).map(function(item) {
-			return {
-				name: item.name,
-				user_id: item.user_id
-			};
-		});
-	},
-
-	runWith: function(cb) {
-		var url = 'https://gist.githubusercontent.com/antidis/c4d7dd067ab47cce5856/raw/7b31c00ee9d52bf817de0708dabad3e5eb62f8ad/locations.json';
-
-		module.exports.fetch(url, function(res) {
-			var filtered = module.exports.filterWithinDistance(res, {lat: 53.3381985, lng: -6.2592576}, 30);
-
-			console.log(module.exports.sortAndMap(filtered));		
-		});
 	}
 
+	var earth_radius = 6371,
+			source_lat_radians = source_coord.lat.toRadians(),
+			dest_lat_radians = dest_coord.lat.toRadians(),
+			lat_diff_radians = (dest_coord.lat - source_coord.lat).toRadians(),
+			lng_diff_radians = (dest_coord.lng - source_coord.lng).toRadians();
+
+	var formula = Math.sin(lat_diff_radians / 2) * Math.sin(lat_diff_radians / 2) +
+								Math.cos(source_lat_radians) * Math.cos(dest_lat_radians) *
+								Math.sin(lng_diff_radians / 2) * Math.sin(lng_diff_radians / 2),
+			ataned = 	2 * Math.atan2(Math.sqrt(formula), Math.sqrt(1 - formula));
+
+	return earth_radius * ataned;
 };
+
+/**
+ *	Filter the fetched data by their distance
+ *
+ *	@param {Array} items - the fetched items from the json endpoint 
+ *	@param {Object} source_location - the reference location used to measure distance
+ *	@param {Number} within_distance - the given distance (in KM) to filter by
+ *	@return {Array} - the items filtered by distance
+ */
+var filterWithinDistance = function(items, source_location, within_distance) {
+	return items.filter(function(item) {
+		if(typeof item.latitude === 'undefined' || typeof item.longitude === 'undefined') {
+			return;
+		}
+		return haversineDistance(source_location, {lat: item.latitude, lng: item.longitude}) <= within_distance;
+	});
+};
+
+/**
+ *	Order and map a collection
+ *	@param {Array} source - the unmapped and unsorted array
+ *	@return {Array} dest - the mapped and sorted array
+ */
+var sortAndMap = function(source) {
+	return source.sort(function(a, b){
+		return a.user_id > b.user_id;		
+	}).map(function(item) {
+		return {
+			name: item.name,
+			user_id: item.user_id
+		};
+	});
+};
+
+module.exports = {
+	fetch: fetch,
+	haversineDistance: haversineDistance,
+	filterWithinDistance: filterWithinDistance,
+	sortAndMap: sortAndMap
+};
+
+// runWith: function(cb) {
+// 	var url = 'https://gist.githubusercontent.com/antidis/c4d7dd067ab47cce5856/raw/7b31c00ee9d52bf817de0708dabad3e5eb62f8ad/locations.json';
+
+// 	module.exports.fetch(url, function(res) {
+// 		var filtered = module.exports.filterWithinDistance(res, {lat: 53.3381985, lng: -6.2592576}, 30);
+
+// 		console.log(module.exports.sortAndMap(filtered));		
+// 	});
+// }
+
--- test/index_unit.js
+++ test/index_unit.js
@@ -2,15 +2,18 @@
 
 var chai = require('chai'),
 		should = chai.should(),
+		assert = chai.assert,
 		expect = chai.expect,
 		geocatcher = require('../index'),
-		haversineDistance = geocatcher.haversineDistance;
+		haversineDistance = geocatcher.haversineDistance,
+		filterWithinDistance = geocatcher.filterWithinDistance,
+		sortAndMap = geocatcher.sortAndMap;
 
 describe('#haversineDistance', function() {
 
 	it('should calculate the correct distance given some coordinates', function() {
 
-		var source_coordinate = {lat: 53.339370, lng: -6.257496},
+		var source_coordinate = {lat: 53.339370, lng: -6.257496}, //Intercom Office
 		test_coordinates = [
 			{lat: 53.375900, lng: -6.096441}, //11.43km Dublin
 			{lat: 53.263588, lng: -9.069186}, //187.01km Galway
@@ -29,13 +32,66 @@ describe('#haversineDistance', function() {
 
 });
 
-// describe('#filterByDistance', function() {
+describe('#filterWithinDistance', function() {
 
-// 	it('returns the correct filtered items given a collection of items and a distance', function() {
-// 		filterByDistance().should.equal(true);
-// 	});
+	it('should return a collection of items only within the specified distance given coordinates', function() {
 
-// });
+		var source_coordinate = {lat: 53.339370, lng: -6.257496}, //Intercom Office
+				test_items = [
+			{latitude: 52.986375, user_id: 12, name: "Christina McArdle", longitude: -6.043701}, 	//41km
+		  {latitude: 51.92893, user_id: 1, 	name: "Alice Cahill", longitude: -10.27699}, 				//313km
+		  {latitude: 51.8856167, user_id: 2, name: "Ian McArdle", longitude: -10.4240951}, 			//326km
+		  {latitude: 52.3191841, user_id: 3, name: "Jack Enright", longitude: -8.5072391}, 			//191km
+		  {latitude: 53.807778, user_id: 28, name: "Charlie Halligan", longitude: -7.714444}, 	//109km
+		  {latitude: 53.4692815, user_id: 7, name: "Frank Kehoe", longitude: -9.436036}, 				//211km
+		  {latitude: 54.0894797, user_id: 8, name: "Eoin Ahearn", longitude: -6.18671}, 				//83km
+		  {latitude: 53.038056, user_id: 26, name: "Stephen McArdle", longitude: -7.653889} 		//99km 
+		];
+
+		expect(filterWithinDistance(test_items, source_coordinate, 50).length).to.equal(1);
+		expect(filterWithinDistance(test_items, source_coordinate, 100).length).to.equal(3);
+		expect(filterWithinDistance(test_items, source_coordinate, 200).length).to.equal(5);
+		expect(filterWithinDistance(test_items, source_coordinate, 300).length).to.equal(6);
+
+	}); 
+
+});
+
+describe('#sortAndMap', function() {
+
+	var test_items = [
+		{latitude: 52.986375, user_id: 12, name: "Christina McArdle", longitude: -6.043701},
+	  {latitude: 51.92893, user_id: 1, 	name: "Alice Cahill", longitude: -10.27699},
+	  {latitude: 51.8856167, user_id: 2, name: "Ian McArdle", longitude: -10.4240951},
+	  {latitude: 52.3191841, user_id: 3, name: "Jack Enright", longitude: -8.5072391},
+	  {latitude: 53.807778, user_id: 28, name: "Charlie Halligan", longitude: -7.714444} 	
+	];
+
+	it('should return a collection sorted by user_id in ascending order', function() {
+
+		var sorted_reference_ids = [1, 2, 3, 12, 28],
+				sorted_test_items = sortAndMap(test_items);
+
+		sorted_test_items.forEach(function(item, index) {
+			expect(item.user_id).to.equal(sorted_reference_ids[index]);
+		});
+
+	});
+
+	it('should correctly map the items properties', function() {
+
+		var sorted_test_items = sortAndMap(test_items);
+
+		sorted_test_items.forEach(function(item) {
+			expect(item.user_id).to.be.a('number');
+			expect(item.name).to.be.a('string');
+			assert.isUndefined(item.latitude);
+			assert.isUndefined(item.longitude);
+		});
+
+	});
+
+});
 
 
 

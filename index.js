cf2d3965f76e675dac19b75e3e655ba039b2ccbc
Matt Finucane <matfin@gmail.com>
2015-07-25 15:58:21 +0200

    Using more accurate formula to calculate distance.

--- index.js
+++ index.js
@@ -21,23 +21,25 @@ module.exports = {
 	/**
 	 *	Calculate distance given a coordinate relative to another
 	 *
-	 *	@param {Object} source_coord - the source coordinate 
+	 *	@param {Object} source_coord - the source coordinate which should take the form {lat: 44.55555, lng: -5.33322}
 	 *	@param {Object} dest_coord - given coordinate to calculate against
 	 *	@return {Number} - the calculated distance
 	 */
-	calculate: function(source_coord, dest_coord) {
-		debugger;
-
-		source_coord = {lat: 53.348069, lng: -6.259360};
-		dest_coord = {lat: 54.602362, lng: -5.918710};
-
+	calculateDistance: function(source_coord, dest_coord) {
 		var earth_radius = 6371,
 				sin_source_lat = Math.sin(source_coord.lat),
 				sin_dest_lat = Math.sin(dest_coord.lat),
 				cos_source_lat = Math.cos(source_coord.lat),
 				cos_dest_lat = Math.cos(dest_coord.lat),
 				cos_diff_lng = Math.cos(Math.abs(source_coord.lng - dest_coord.lng)),
-				degrees = Math.acos((sin_source_lat * sin_dest_lat) + (cos_source_lat * cos_dest_lat * cos_diff_lng)),
+				sin_diff_lng = Math.sin(Math.abs(source_coord.lng - dest_coord.lng)),
+
+		 		f1 = Math.pow(cos_dest_lat * sin_diff_lng, 2),
+				f2 = Math.pow((cos_source_lat * sin_dest_lat) - (sin_source_lat * cos_dest_lat * cos_diff_lng), 2),
+				f3 = (sin_source_lat * sin_dest_lat),
+				f4 = (cos_source_lat * cos_dest_lat * cos_diff_lng),
+				degrees = Math.atan2(Math.sqrt(f1 + f2), f3 + f4),
+
 				radians = degrees * (Math.PI / 180);
 
 		return earth_radius * radians;
